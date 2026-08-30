4fd3be871a312ea891814f61f84f1ca429d3e109
Zachary U <zacharybbu@gmail.com>
2026-08-30 16:09:13 -0500

    Update messages.js

--- messages.js
+++ messages.js
@@ -1,9 +1,16 @@
 const MESSAGES = [
   {
+    title: "Ephesians 2:14–18 | The Peace of Christ",
+    venue: "Bethel Baptist Church",
+    date: "June 7, 2026",
+    duration: "42:44",
+    audioSrc: "Ephesians 2.14–18.m4a"
+  },
+  {
     title: "Ephesians 3:1–6 | The Mystery of Christ Revealed",
     venue: "Bethel Baptist Church",
     date: "August 8, 2026",
-    duration: "20:30",
+    duration: "42:15",
     audioSrc: "Ephesians 3.1–6.mp3"
   }
 ];
