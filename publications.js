1be6fd7581deccda004f91586d719d42cf351767
Zachary U <zacharybbu@gmail.com>
2026-08-30 15:41:04 -0500

    Create publications.js

---
+++ publications.js
@@ -0,0 +1,11 @@
+const PUBLICATIONS = [
+  {
+    title: "Passing Over the Bloody Bridegroom: Exodus 4:24-26 in Its Biblical-Theological Context",
+    journal: "The Master's University Journal of Biblical and Theological Studies",
+    volume: "Volume 1",
+    year: "Spring 2024",
+    pages: "3–16",
+    desc: "Analyzes the strange and graphic narrative of Exodus 4:24–26, demonstrating how Moses experienced a personal Passover through his son's circumcision that prefigures and sets the paradigm for Israel's national Passover.",
+    pdfSrc: "Passing Over the Bloody Bridegroom.pdf"
+  }
+];
