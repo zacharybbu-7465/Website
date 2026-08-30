b37c3cb32a456287d899a2288b2dd65990ab4660
Zachary U <zacharybbu@gmail.com>
2026-08-30 14:34:05 -0500

    Create messages.js

---
+++ messages.js
@@ -0,0 +1,9 @@
+const MESSAGES = [
+  {
+    title: "Ephesians 3:1–6 | The Mystery of Christ Revealed",
+    venue: "Bethel Baptist Church",
+    date: "August 8, 2026",
+    duration: "20:30",
+    audioSrc: "Ephesians 3.1–6.mp3"
+  }
+];
